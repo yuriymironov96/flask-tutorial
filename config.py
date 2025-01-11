86e149079083ca22c982a31ca55aa095c7c9902f
Yurii Mironov <yuriymironov96@gmail.com>
2025-01-11 20:43:20 +0200

    complete chapter 4

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ load_dotenv(os.path.join(basedir, '.flaskenv'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
