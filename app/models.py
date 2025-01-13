12521322885f39aa7a9b3793371662dc0fc6d59e
Yurii Mironov <yuriymironov96@gmail.com>
2025-01-13 20:23:40 +0200

    complete chapter 10

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from time import time
+import jwt
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -5,7 +7,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import app, db, login
 
 
 followers = sa.Table(
@@ -92,6 +94,20 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except Exception:
+            return
+        return db.session.get(User, id)
 
 
 class Post(db.Model):
